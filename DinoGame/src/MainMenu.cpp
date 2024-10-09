7ef158f9b92e66420563263d97e4d7932d95e3e6
ZAsabiha <166790186+ZAsabiha@users.noreply.github.com>
2024-10-09 20:09:52 +0600

    Update MainMenu.cpp

--- DinoGame/src/MainMenu.cpp
+++ DinoGame/src/MainMenu.cpp
@@ -1,44 +1,41 @@
 #include <iostream>
-#include <cstdlib>  
+#include <conio.h>  
 #include <windows.h>
 
 using namespace std;
 
-void instructions() {
+void displayInstructions() {
     system("cls");
-    cout << "Instructions";
-    cout << "\n----------------";
-    cout << "\n1. Avoid hurdles by jumping";
-    cout << "\n2. Press 'Spacebar' to jump ";
-    cout << "\n3. Press 'p' to pause game ";
-    cout << "\n4. Press 'Escape' to exit from game";
-    cout << "\n\nPress any key to go back to menu";
+    cout << "Game Instructions";
+    cout << "\n-----------------------------";
+    cout << "\n1. Jump over obstacles to avoid collisions.";
+    cout << "\n2. Use the 'Spacebar' to make your Dino jump.";
+    cout << "\n3. Press 'P' to pause the game at any time.";
+    cout << "\n4. Press 'Escape' to quit the game and return to the main menu.";
+    cout << "\n\nGood luck, and have fun!";
+    cout << "\n\nPress any key to return to the main menu.";
     getch();
 }
+
 int main() {
-    int choice;
+    setcursor(0, 0);
 
-    displayMenu();
-    cin >> choice;
+    do {
+        system("cls");
+        gotoxy(10, 5); cout << " -------------------------- ";
+        gotoxy(10, 6); cout << " |        DINO RUN        | ";
+        gotoxy(10, 7); cout << " -------------------------- ";
+        gotoxy(10, 9); cout << "1. Play Dino Run Game";
+        gotoxy(10, 10); cout << "2.View Instructions";
+        gotoxy(10, 11); cout << "3.Exit Game";
+        gotoxy(10, 13); cout << "Please Select an option: ";
+        char userinput = getche();
 
-    switch (choice) {
-        case 1:
-            cout << "Starting the game..." << endl;
-            break;
-        case 2:
-            cout << "Restarting the game..." << endl;
-            break;
-        case 3:
-            cout << "Opening settings..." << endl;
-            break;
-        case 4:
-            cout << "Exiting the game..." << endl;
-            break;
-        default:
-            cout << "Invalid option." << endl;
-            break;
-    }
+        if (userinput == '1') play();
+        else if (userinput == '2') instructions();
+        else if (userinput == '3') exit(0);
+
+    } while (1);
 
     return 0;
 }
-
