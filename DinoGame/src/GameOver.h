b29fbe547aba7b7b618a089e8c524a747b2ed7a5
Zannatul Adon Sabiha <166790186+ZAsabiha@users.noreply.github.com>
2025-03-11 15:36:02 +0600

    Update GameOver.h

--- DinoGame/src/GameOver.h
+++ DinoGame/src/GameOver.h
@@ -1,1 +1,17 @@
 
+
+#ifndef GAMEOVER_H
+#define GAMEOVER_H
+
+#include <iostream>
+#include <conio.h> 
+#include "GameLogic.h" 
+
+class GameOver {
+public:
+
+    void displayGameOver();
+  
+};
+
+#endif 
