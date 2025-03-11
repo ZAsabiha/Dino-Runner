cb7e06585073e192b15ab1558e7298eacbb5fd54
Zannatul Adon Sabiha <166790186+ZAsabiha@users.noreply.github.com>
2025-03-11 15:33:10 +0600

    Update GameLogic.h

--- DinoGame/src/GameLogic.h
+++ DinoGame/src/GameLogic.h
@@ -1,1 +1,19 @@
 
+#ifndef GAMELOGIC_H
+#define GAMELOGIC_H
+
+#include <string>
+
+class GameLogic {
+    public:
+    
+    
+; 
+    void drawHurdle(int missileX, int missileY, bool &missileActive, bool isDucking);
+    void fireProjectile(int &missileX, int &missileY, bool &missileActive, bool isVertical);
+    void moveProjectile(int &missileX, int &missileY, bool &missileActive, bool isVertical);
+    void drawProjectile(int missileX, int missileY, bool missileActive, bool isVertical);
+};
+
+#endif 
+
