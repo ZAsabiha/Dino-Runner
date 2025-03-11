8f27742b3355ebae828ed86d2ba85a9cdce49b9b
Zannatul Adon Sabiha <166790186+ZAsabiha@users.noreply.github.com>
2025-03-11 15:31:12 +0600

    Create GameControl.h

---
+++ DinoGame/src/GameControl.h
@@ -0,0 +1,19 @@
+#ifndef GAMECONTROL_H
+#define GAMECONTROL_H
+
+
+
+class GameControl {
+    public:
+ 
+    
+       
+
+  
+ 
+        
+        void init();    
+        void play();   
+};
+
+#endif  
