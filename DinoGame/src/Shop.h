3d3f90bea4bb10a4802253ae74a723aab403aba7
IRIvey <163230058+IRIvey@users.noreply.github.com>
2025-03-17 20:07:38 +0600

    Add files via upload

---
+++ DinoGame/src/Shop.h
@@ -0,0 +1,17 @@
+#ifndef SHOP_H
+#define SHOP_H
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include "Utility.h"
+
+class Shop {
+public:
+    
+
+    // Function to display the shop menu and handle item purchases
+    void shopMenu();
+};
+
+#endif // SHOP_H
