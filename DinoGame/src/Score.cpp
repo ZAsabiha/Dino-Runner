329dc6ebf4e8434fa519f4dd1373f1442e14a574
Zannatul Adon Sabiha <166790186+ZAsabiha@users.noreply.github.com>
2025-03-21 09:48:44 +0600

    Update Score.cpp

--- DinoGame/src/Score.cpp
+++ DinoGame/src/Score.cpp
@@ -1,26 +1,29 @@
+
 #include "Score.h"
 #include <fstream> 
 #include <iostream>
 #include <conio.h> 
 #include <windows.h>
 #include "Utility.h"
+#include "GameLogic.h"
+#include "Global.h"
 int gameover=0;
-Score::Score() : currentScore(0), highScore(0) {}
+Score::Score()  {}
 
 void Score::loadHighScore() {
     std::ifstream file("highscore.txt");
     if (file.is_open()) {
-        file >> highScore;
+        file >> gameState.highScore;
         file.close();
     } else {
-        highScore = 0; 
+        gameState.highScore = 0; 
     }
 }
 
 void Score::saveHighScore() {
     std::ofstream file("highscore.txt");
     if (file.is_open()) {
-        file << highScore;
+        file << gameState.highScore;
         file.close();
     }
 }
@@ -36,15 +39,14 @@ void Score::displayScore(int currentScore) {
     
     
     Utility::gotoxy(3, 4);
-    std::cout << "\033[1;36mHIGH SCORE: \033[0m" << highScore;
+    std::cout << "\033[1;36mHIGH SCORE: \033[0m" << gameState.highScore;
 }
 
 int Score::getHighScore() const {
-    return highScore;
+    return gameState.highScore;
 }
 
 void Score::setHighScore(int newHighScore) {
-    highScore = newHighScore;
+    gameState.highScore = newHighScore;
 }
-
 
